0da2d7aa9da2d24cc2226478145396920f7a6a03
Xinhu Liu <xinhu.liu90@gmail.com>
2021-02-07 14:57:15 +0100

    Add class ConfigLoader

--- src/config.js
+++ src/config.js
@@ -1,20 +1,45 @@
-import * as fs from "fs"
+import * as fs from 'fs';
 
-let config;
+export class ConfigLoader {
+  #config;
 
-/**
- *
- * @param {string} path
- */
-export function loadConfig(path) {
-  const content = fs.readFileSync(path).toString();
-  config = JSON.parse(content);
-}
+  constructor(configPath) {
+    const fileContent = fs.readFileSync(configPath).toString();
+    this.#config = JSON.parse(fileContent);
+  }
 
-/**
- * @returns {object}
- */
-export function getConfig() {
-  // return a copy of config object
-  return JSON.parse(JSON.stringify(config));
+  /**
+   * Returns the value of the given path. If the value does not exist, an error is thrown.
+   * @param {string} fieldPath
+   */
+  getValue(fieldPath) {
+    const pathKeys = fieldPath.split('.');
+
+    return pathKeys.reduce((configObject, key) => {
+      const nextObject = configObject[key];
+
+      if (nextObject === undefined) {
+        throw new Error(`${fieldPath} not found`);
+      }
+
+      return configObject[key];
+    }, this.#config);
+  }
+
+  /**
+   * Returns the string value of the given path. If the value does not exist or the value is not a string, an error is
+   * thrown.
+   * @param {string} fieldPath
+   */
+  getString(fieldPath) {
+    const value = this.getValue(fieldPath);
+
+    if (typeof value != 'string') {
+      throw new Error(`${fieldPath} is not a string.`);
+    }
+
+    return value;
+  }
+
+  // getValueOrDefualt(fieldPath, defaultValue) { }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import * as path from "path";
-import * as url from "url";
-import { getConfig, loadConfig } from "./config.js";
+import * as path from 'path';
+import * as url from 'url';
+import { ConfigLoader } from './config.js';
 
 const currentDir = path.dirname(url.fileURLToPath(import.meta.url));
-const configFilePath = path.join(currentDir, "../config.json")
-loadConfig(configFilePath);
-console.log(getConfig())
+const configFilePath = path.join(currentDir, '../config.json')
+
+const configLoader = new ConfigLoader(configFilePath);
+const config = {
+  consumerKey: configLoader.getString('consumerKey'),
+  consumerSecret: configLoader.getString('consumerSecret'),
+  geocoordinates: configLoader.getString('geocoordinates')
+};
+
+console.log(config)
